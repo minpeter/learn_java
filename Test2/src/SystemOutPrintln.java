bf36582ab16b32544374d34c32d0af19b9c49b6b
minpeter <kali2005611@gmail.com>
2022-03-10 09:10:47 +0900

    주석 연습

--- Test2/src/SystemOutPrintln.java
+++ Test2/src/SystemOutPrintln.java
@@ -1,3 +1,10 @@
+/*
+    파일이름: BlockComment.java
+    작성자: 민웅기
+    작성일: 2012.09.25
+    작성이유: System.out.println 메소드 기능 테스트
+*/
+
 public class SystemOutPrintln {
     public static void main(String[] args) throws Exception {
         System.out.println(7);
