a92ae0977e57ae0bdbd0b22001e35046d86ba5f9
minpeter <kali2005611@gmail.com>
2022-03-14 09:22:09 +0900

    Type class byte and long done.

---
+++ test/src/ByteTest.java
@@ -0,0 +1,7 @@
+public class ByteTest {
+  public static void main(String[] args) {
+    byte num = 10;
+    num += 1;
+    System.out.println(num);
+  }
+}
---
+++ test/src/LongTest.java
@@ -0,0 +1,7 @@
+public class LongTest {
+  public static void main(String[] args) {
+    long num = 123412359832L;
+    num += 1;
+    System.out.println(num);
+  }
+}
