e9717baa424dd35f3c4aeebfb2ed2b2cff0b2ce3
minpeter <kali2005611@gmail.com>
2022-08-25 10:49:35 +0900

    0825 start

---
+++ 2st_semester/java_Day1/src/Kahoot.java
@@ -0,0 +1,6 @@
+public class Kahoot {
+    public static void main(String[] args) {
+        System.out.println("3 * 5 % 6 = " + 3*5%6);
+        System.out.println("8 / 6 = " + 8/6*10.);
+    }
+}
---
+++ 2st_semester/java_Day1/src/SuperObject.java
@@ -0,0 +1,2 @@
+public class SuperObject {
+}
